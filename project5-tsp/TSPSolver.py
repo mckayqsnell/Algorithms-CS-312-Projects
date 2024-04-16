7d2eb324928d74d130d1ca4015a72b0d485100c1
McKay Snell <mckayqsnell@gmail.com>
2024-04-15 23:55:10 -0600

    project 6

--- project5-tsp/TSPSolver.py
+++ project5-tsp/TSPSolver.py
@@ -85,45 +85,53 @@ class TSPSolver:
 		start_time = time.time()
 		cities = self._scenario.getCities()
 		ncities = len(cities)
-		visted = [False] * ncities # list of visited cities
-		route = []
+		overall_best_cost = np.inf
+		overall_best_solution = None
+		
+		for starting_city_index in range(ncities):
+			visted = [False] * ncities # list of visited cities
+			route = []
+			currrent_city_index = starting_city_index
+			visted[currrent_city_index] = True
+			route.append(cities[currrent_city_index])
 
-		# start at the first city (could be any city)
-		currrent_city_index = 0
-		visted[currrent_city_index] = True
-		route.append(cities[currrent_city_index])
+			# loop for visting all cities
+			# O(n^2)
+			while len(route) < ncities and time.time() - start_time < time_allowance:
+				nearest_city_index = None
+				min_distance = np.inf
 
-		# loop for visting all cities
-		# O(n^2) where n is the number of cities
-		while len(route) < ncities:
-			nearest_city_index = None
-			min_distance = np.inf
+				# Loop for finding the nearest city
+				#O(n) where n is the number of cities
+				for i in range(ncities):
+					if not visted[i]:
+						# get the distance from the current city to the city i
+						distance = cities[currrent_city_index].costTo(cities[i])
+						# update the nearest city if the distance is smaller
+						if distance < min_distance:
+							min_distance = distance
+							nearest_city_index = i
 
-			# Loop for finding the nearest city
-			#O(n) where n is the number of cities
-			for i in range(ncities):
-				if not visted[i]:
-					# get the distance from the current city to the city i
-					distance = cities[currrent_city_index].costTo(cities[i])
-					# update the nearest city if the distance is smaller
-					if distance < min_distance:
-						min_distance = distance
-						nearest_city_index = i
-
-			# If no nearest city found, break the loop
-			if nearest_city_index is None:
-				break # No Solution
-			else: 
-				# update the current city and add it to the route
-				currrent_city_index = nearest_city_index
-				visted[currrent_city_index] = True
-				route.append(cities[currrent_city_index])
-
-		# Check if we made it back to the start
-		if route[-1].costTo(route[0]) == np.inf:
+				# If no nearest city found, break the loop
+				if nearest_city_index is None:
+					break # no solution from city, try another city
+				else: 
+					# update the current city and add it to the route
+					currrent_city_index = nearest_city_index
+					visted[currrent_city_index] = True
+					route.append(cities[currrent_city_index])
+			if len(route) == ncities and route[-1].costTo(route[0]) != np.inf:
+				bssf = TSPSolution(route)
+				print(f"Found a solution in greedy with cost: {bssf.cost}")
+				if bssf.cost < overall_best_cost:
+					overall_best_cost = bssf.cost
+					overall_best_solution = bssf
+		
+		if(overall_best_solution is None):
+			print("No solution found in greedy with any starting city")
 			bssf = None
-		else: 
-			bssf = TSPSolution(route)
+		else:
+			bssf = overall_best_solution
 
 		end_time = time.time()
 		results = {
@@ -138,32 +146,21 @@ class TSPSolver:
 
 		return results
 
-
-
-	''' <summary>
-		This is the entry point for the branch-and-bound algorithm that you will implement
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution,
-		time spent to find best solution, total number solutions found during search (does
-		not include the initial BSSF), the best solution found, and three more ints:
-		max queue size, total number of states created, and number of pruned states.</returns>
-	'''
-
 	def branchAndBound( self, time_allowance=60.0):
 		start_time = time.time()
-		self.bssf = self.initialize_bssf()
+		bssf = self.initialize_bssf()
 
 		cities = self._scenario.getCities()
 		n = len(cities)
 		initial_matrix = self.create_initial_matrix()
 		initial_reduced_matrix, lower_bound = self.reduce_matrix(initial_matrix)
-
+		
 		# make the root node
 		root_node = Node(level=0, path=[0], reduced_matrix=initial_reduced_matrix, cost=lower_bound)
 
 		# create a priority queue
 		pq = []
-		heapq.heappush(pq, (lower_bound, -1, root_node))
+		heapq.heappush(pq, (lower_bound, 0, root_node))
 
 		# initialize counters
 		num_solutions = 0
@@ -176,43 +173,38 @@ class TSPSolver:
 			# get the node with the lowest bound
 			_ , _ , current_node = heapq.heappop(pq)
 
-			# if current node's cost is less than the bssf, explore it
-			if current_node.cost < self.bssf.cost:
-				# if the path is complete, update the bssf
-				if len(current_node.path) == n:
-					# add the cost of the edge back to the start city
-					cost_of_edge_back_to_start = current_node.reduced_matrix[current_node.path[-1], current_node.path[0]]
-					complete_cost =  current_node.cost + cost_of_edge_back_to_start
-					# update the bssf
-					if complete_cost < self.bssf.cost or self.bssf is None:
-						new_path = current_node.path + [current_node.path[0]]
-						self.bssf = TSPSolution([cities[i] for i in new_path])
-						num_solutions += 1
-				else:
-					# create children for each city not yet visited
-					for new_city_index in range(1, n):
-						# if the city is not in the path
-						if new_city_index not in current_node.path:
-							# create a child node
-							child_node = self.create_child_node(current_node, new_city_index)
-							num_states_created += 1
-							# if the child's cost is less than the bssf, add it to the queue
-							if child_node.cost < self.bssf.cost:
-								heapq.heappush(pq, (child_node.cost, -child_node.level, child_node))
-							else:
-								num_pruned_states += 1
-				
-				# update the max queue size
-				max_queue_size = max(max_queue_size, len(pq))
-			else:
+			if current_node.cost >= bssf.cost or len(current_node.path) > n:
 				num_pruned_states += 1
+				continue
+
+			if len(current_node.path) == n and current_node.reduced_matrix[current_node.path[-1], 0] != np.inf:
+				complete_cost = current_node.cost + current_node.reduced_matrix[current_node.path[-1], 0]
+				if complete_cost < bssf.cost:
+					new_path = current_node.path
+					bssf = TSPSolution([cities[i] for i in new_path])
+					num_solutions += 1
+				continue
+
+			# O(n)
+			for new_city_index in range(1, n):
+				if new_city_index not in current_node.path:
+					child_node = self.create_child_node(current_node, new_city_index)
+					num_states_created += 1
+
+					if child_node.cost < bssf.cost and child_node.cost != np.inf:
+						# inverted the level to prioritize nodes with lower cost and higher depth
+						heapq.heappush(pq, (child_node.cost, -child_node.level, child_node))
+					else:
+						num_pruned_states += 1
+			# O(n)?
+			max_queue_size = max(max_queue_size, len(pq))
 
 		end_time = time.time()
 		results = {
-			'cost': self.bssf.cost,
+			'cost': bssf.cost if bssf is not None else math.inf,
 			'time': end_time - start_time,
 			'count': num_solutions,
-			'soln': self.bssf,
+			'soln': bssf,
 			'max': max_queue_size,
 			'total': num_states_created,
 			'pruned': num_pruned_states,
@@ -220,80 +212,60 @@ class TSPSolver:
 
 		return results
 
+	# O(n^2)
 	def create_initial_matrix(self):
 		cities = self._scenario.getCities()
 		n = len(cities)
-		matrix = np.full((n, n), np.inf)
+		# if an edge doesn't exist, it's infinity
+		matrix = np.full((n, n), np.inf) 
 
+		# dont assume symmetric matrix
 		for i in range(n):
-			for j in range(i+1, n):
+			for j in range(n):
 				if i != j:
 					cost = cities[i].costTo(cities[j])
-					matrix[i, j] = matrix[j, i] = cost
+					matrix[i, j] = cost
 
 		return matrix
-
+	
+	# O(n^2) --> because of greedy
 	def initialize_bssf(self):
 		# Run the greedy algorithm to get the initial BSSF
 		results = self.greedy()
 		bssf = results['soln']
 
-		# If the greedy algorithm did not find a solution, run the default random tour
-		if bssf is None or bssf.cost == np.inf:
-			results = self.defaultRandomTour()
-			bssf = results['soln']
-		
 		return bssf
 	
+	# O(n^2)
 	def reduce_matrix(self, matrix):
 		# Reduce rows
-		# n = matrix.shape[0]
-
-		# # Reduce rows
-		# # O(n^2) where n is the number of cities (rows)
-		# for i in range(n):
-		# 	row_min = np.min(matrix[i, :]) # O(n)
-		# 	# prevent extra operations
-		# 	if(row_min != np.inf and row_min != 0):
-		# 		matrix[i, :] -= row_min
-		# 		bound += row_min
-
-		# # Reduce columns
-		# # O(n) where n is the number of cities (columns)
-		# for j in range(n):
-		# 	col_min = np.min(matrix[:, j]) # O(n)
-		# 	# prevent extra operations
-		# 	if col_min != np.inf and col_min != 0:
-		# 		matrix[:, j] -= col_min
-		# 		bound += col_min
-  
-		# Reduce rows
 		bound = 0
-		row_min = np.min(matrix, axis=1, keepdims=True)  # Find min in rows
+		# row_min matrix (1D array)
+		row_min = np.min(matrix, axis=1, keepdims=True)
 		for i in range(len(row_min)):
+			# Subtract row min only from finite values
 			if np.isfinite(row_min[i]):
 				matrix[i, :] -= row_min[i]
-		# Subtract row min only from finite values
-		bound = np.sum(row_min[np.isfinite(row_min)])  # Sum of row reductions, excluding inf
+		# Sum of row reductions, excluding inf
+		bound = np.sum(row_min[np.isfinite(row_min)])
 
 		# Reduce columns
-		col_min = np.min(matrix, axis=0)  # Find min in columns
+		# col_min matrix(1D array)
+		col_min = np.min(matrix, axis=0)
 		for j in range(len(col_min)):
 			if np.isfinite(col_min[j]):
 				matrix[:, j] -= col_min[j]
-		bound += np.sum(col_min[np.isfinite(col_min)])  # Sum of column reductions, excluding inf
+		# Sum of column reductions, excluding inf
+		bound += np.sum(col_min[np.isfinite(col_min)])
 
 		return matrix, bound
 	
+	# O(n^2)?
 	def create_child_node(self, parent_node, new_city_index):
 		# Copy over the parent's reduced matrix
 		new_matrix = np.copy(parent_node.reduced_matrix) # O(n^2)
-		# print("New Matrix before reduction: ")
-		# print(new_matrix)
-
 		# where I came from (row: from)
 		new_matrix[parent_node.path[-1], :] = np.inf # O(n)
-
 		# can't go back to where I came from (column: to)
 		new_matrix[:, new_city_index] = np.inf # O(n)
 
@@ -302,22 +274,18 @@ class TSPSolver:
 		if(len(parent_node.path) > 1):
 			new_matrix[new_city_index, parent_node.path[-1]] = np.inf # O(n)
 
-		# Reduce the new matrix and update the bound
 		new_matrix, reduction_cost = self.reduce_matrix(new_matrix) # O(n^2)
-		# cost of og bound + new cost + cost of taking the edge
-		cost_of_edge = parent_node.reduced_matrix[parent_node.path[-1], new_city_index] if parent_node.path[-1] != new_city_index else 0
-		new_bound = parent_node.cost + reduction_cost + cost_of_edge
 
+		cost_of_edge = parent_node.reduced_matrix[parent_node.path[-1], new_city_index]
+		new_bound = parent_node.cost + reduction_cost + cost_of_edge
+  		# O(n) since it's a list append
+		new_path = parent_node.path[:] + [new_city_index]
 		# new node with new info
-		new_path = parent_node.path[:] + [new_city_index] # O(n) since it's a list append
-		# new_path = parent_node.path.copy().append(new_city_index)
-		# print("Creating child node with path:", new_path)
 		new_node = Node(level=parent_node.level + 1, path=new_path, reduced_matrix=new_matrix, cost=new_bound, parent=parent_node)
+
 
 		return new_node
   
-
-
 # For easibility with keeping track of the branch and bound tree
 class Node:
 	def __init__(self, level, path, reduced_matrix, cost, parent=None):
@@ -327,6 +295,9 @@ class Node:
 		self.cost = cost  # Lower bound of the path cost
 		self.parent = parent  # Reference to parent node
 	def __lt__(self, other):
+		# priortize nodes with lower cost and higher dpeth when they have the same cost
+		if self.cost == other.cost:
+			return self.level > other.level
 		return self.cost < other.cost
 
 # For testing purposes
